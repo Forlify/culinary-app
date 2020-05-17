7220124d9d2e0904375f8a51b1448e5ce02776cb
piotrpolec <piotr.jan.polec@gmail.com>
2020-05-17 19:12:55 +0200

    Almost finished

--- recipes/models.py
+++ recipes/models.py
@@ -23,6 +23,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+    
+    def return_category(self):
+        return str(self.category)
 
 
 class Ingredient(models.Model):
@@ -52,8 +55,11 @@ class IngredientInstance(models.Model):
     def __str__(self):
         return self.ingredient.name + " " + str(self.how_much) + " " + self.how_much_of_what
 
+    def return_measurement(self):
+        return self.Measurement.choices
 
-class Steps(models.Model):
+
+class Step(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.DO_NOTHING)
     number = models.IntegerField()
     text = models.CharField(max_length=200)
